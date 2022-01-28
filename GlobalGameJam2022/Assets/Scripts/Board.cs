c44161d42f13b4737e38f6a6e14f158af4edee2d
PyreticDev <80097192+PyreticDev@users.noreply.github.com>
2022-01-28 20:59:40 +0000

    Added movement

---
+++ GlobalGameJam2022/Assets/Scripts/Board.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Board : MonoBehaviour
+{
+    public Vector2 boardSquareSize = new Vector2(200, 130);
+}
