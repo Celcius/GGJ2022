4ddafefda042031de188704266b363a3f82c4ec7
DESKTOP-1TF8JDR\ivoca <ivo.capelo@gmail.com>
2022-01-30 16:05:42 +0000

    Added Icon

--- GlobalGameJam2022/Assets/Scripts/LevelManager.cs
+++ GlobalGameJam2022/Assets/Scripts/LevelManager.cs
@@ -101,7 +101,9 @@ public class LevelManager : Singleton<LevelManager>
 
         if(Input.GetKeyUp(exitKey)) {
             Application.Quit();
+#if UNITY_EDITOR            
             UnityEditor.EditorApplication.isPlaying = false;
+#endif
         }
     }
 
@@ -138,10 +140,10 @@ public class LevelManager : Singleton<LevelManager>
 
     private IEnumerator LoadLevelRoutine(int index)
     {
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
         LevelInstantiator.Instance.InstantiateLevel(_levels.Value[index]);
         _board.SetBoardIndex(0);
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(2.0f);
         EnableInput();
     }
 
